8a61ebedadbb1968fdbb233831e4045829dd8b81
Thomas <34007164+kai7win@users.noreply.github.com>
2023-09-16 14:22:34 +0800

    NextView

---
+++ ThemeManager/NextViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NextViewController.swift
+//  ThemeManager
+//
+//  Created by Thomas on 2023/9/16.
+//
+
+import UIKit
+
+class NextViewController: UIViewController,Themed {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ThemeManager.shared.subscribe(self)
+        
+        self.view.backgroundColor = ThemeManager.shared.theme.navColor
+    }
+    
+    func applyTheme(_ theme: Theme) {
+        self.view.backgroundColor = ThemeManager.shared.theme.navColor
+    }
+
+}
